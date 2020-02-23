ee725736a32caac6ea9d7c0d6403cf0584c9bc82
Israel Motta <cerqueiramotta@gmail.com>
2020-02-22 23:02:39 -0300

    commit

--- build/husky/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/isadora/desafio_husky/src/husky
-# Build directory: /home/isadora/desafio_husky/build/husky
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/navigation_2d/nav2d_operator/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/isadora/desafio_husky/devel/include;/home/isadora/desafio_husky/src/navigation_2d/nav2d_operator/include".split(';') if "/home/isadora/desafio_husky/devel/include;/home/isadora/desafio_husky/src/navigation_2d/nav2d_operator/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "roscpp;message_runtime;tf;costmap_2d;sensor_msgs".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "-lRobotOperator".split(';') if "-lRobotOperator" != "" else []
-PROJECT_NAME = "nav2d_operator"
-PROJECT_SPACE_DIR = "/home/isadora/desafio_husky/devel"
-PROJECT_VERSION = "0.4.2"
